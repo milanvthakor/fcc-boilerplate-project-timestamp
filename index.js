07ea6eb420b1f2b82f175317ef2095943709603f
milanthakor_crest <milan.thakor@crestdatasys.com>
2022-05-17 09:16:52 +0530

    Completed the challenge.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,13 +18,29 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// Generate the object that will be returned to the client as a response
+const generateResponse = (date) => ({
+  unix: date.getTime(),
+  utc: date.toUTCString()
 });
 
-
+// API endpoint for unix timestamp
+app.get('/api/:providedDate?', (req, res) => {
+  let { providedDate } = req.params;
+  if (providedDate && providedDate !== '') {
+    let date = new Date(
+      // 5 digits or more must be timestamp, hence parse it into Number
+      /\d{5,}/.test(providedDate) ? parseInt(providedDate) : providedDate
+    );
+    if (isNaN(date)) {
+      res.json({ error: 'Invalid Date' });
+    } else {
+      res.json(generateResponse(date));
+    }
+  } else {
+    res.json(generateResponse(new Date()));
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
